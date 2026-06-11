6f443bdb4b03a1c3c4e5b504109ecd7da5154a9c
Rupak <your-github-onev523@gmail.com>
2026-06-11 23:00:46 +0530

    Test createOrder

--- api/createOrder.js
+++ api/createOrder.js
@@ -1,42 +1,8 @@
-console.log("Create Order API Loaded");
-console.log("KEY:", process.env.RAZORPAY_KEY_ID);
 const Razorpay = require("razorpay");
 
 module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      message: "Method not allowed"
-    });
-  }
-
-  try {
-    const razorpay = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_SECRET
-    });
-
-    const amount = Number(req.body.amount);
-
-    if (!amount || amount <= 0) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid amount"
-      });
-    }
-
-    const order = await razorpay.orders.create({
-      amount: amount * 100,
-      currency: "INR",
-      receipt: "receipt_" + Date.now()
-    });
-
-    return res.status(200).json(order);
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      success: false,
-      error: error.message
-    });
-  }
+  res.status(200).json({
+    success: true,
+    message: "API working"
+  });
 };
